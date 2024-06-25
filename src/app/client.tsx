e1a93527eefffd9956eb988ceab94925dc4ce7ab
Khuda Dad Nomani <khudadad414@gmail.com>
2024-06-25 12:57:21 +0100

    test

---
+++ src/app/client.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React from 'react'
+
+function Client() {
+  console.log('hello from client component.')
+  return (
+    <div>From Client</div>
+  )
+}
+
+export default Client
